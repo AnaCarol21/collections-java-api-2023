de281e6f15e915c5dc3eac637099597c37d9c2e5
Ana Lima <carol.agl@live.com>
2024-05-13 11:44:14 -0300

    add: solucao do exercicio de conjunto de palavras unicas

--- src/set/operacoesBasicas/PalavrasUnicas/ConjuntoPalavrasUnicas.java
+++ src/set/operacoesBasicas/PalavrasUnicas/ConjuntoPalavrasUnicas.java
@@ -1,5 +1,49 @@
 package set.operacoesBasicas.PalavrasUnicas;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ConjuntoPalavrasUnicas {
+    private Set<String> palavrasSet;
+
+    public ConjuntoPalavrasUnicas(){
+        this.palavrasSet = new HashSet<>();
+    }
+
+    public void adicinarPalavra(String palavra){
+        palavrasSet.add(new String(palavra));
+    }
+
+    public void removerPalavra(String palavra){
+            if(palavrasSet.contains(palavra))
+                palavrasSet.remove(palavra);            
+    }
+
+    public void verificarPalavra(String palavra){
+        if(palavrasSet.contains(palavra)){
+            System.out.println("A palavra: "+palavra+" está presente no conjunto");
+        }
+    }
+
+    public void exibirPalavrasUnicas(){
+        System.out.println(palavrasSet);
+    }
+
+    public static void main(String[] args) {
+        ConjuntoPalavrasUnicas cpu = new ConjuntoPalavrasUnicas();
+
+        cpu.adicinarPalavra("Abacate");
+        cpu.adicinarPalavra("Romã");
+        cpu.adicinarPalavra("Pessego");
+        cpu.adicinarPalavra("Morango");
+        cpu.adicinarPalavra("Morango");
+        
+        cpu.removerPalavra("Abacate");
+
+        cpu.verificarPalavra("Morango");
+
+        cpu.exibirPalavrasUnicas();
+
+    }
     
 }
